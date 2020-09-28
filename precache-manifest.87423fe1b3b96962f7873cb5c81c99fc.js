e20b5cab6826475c1c065ca5a31229021d2a9013
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-09-28 10:44:08 +0900

    Updates

---
+++ precache-manifest.87423fe1b3b96962f7873cb5c81c99fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b52dee6c1da339f6566c514928bb71a",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "50662baa0fb601136ff9",
+    "url": "/habit_tracker/static/css/main.fb3b6831.chunk.css"
+  },
+  {
+    "revision": "299c612c19c98217c710",
+    "url": "/habit_tracker/static/js/2.6982bc93.chunk.js"
+  },
+  {
+    "revision": "1ea337ab0f6eaf802a045a4531cad9ed",
+    "url": "/habit_tracker/static/js/2.6982bc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50662baa0fb601136ff9",
+    "url": "/habit_tracker/static/js/main.21fc1001.chunk.js"
+  },
+  {
+    "revision": "b9f33d5c3a47a69f892b",
+    "url": "/habit_tracker/static/js/runtime-main.39175f41.js"
+  }
+]);
